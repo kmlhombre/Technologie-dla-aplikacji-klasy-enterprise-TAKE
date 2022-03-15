2d1af5e017277e627e8f9ff79fcb69d54e9e9ce4
Kamil Osak <kamil.osak@student.put.poznan.pl>
2022-03-15 18:57:16 +0100

    lab1

---
+++ Serwerlety Java, JSP, JSTL/ServletsJSPJSTL/src/java/ai/beans/ColorBean.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ai.beans;
+
+/**
+ *
+ * @author student
+ */
+public class ColorBean {
+    
+    private String foregroundColor;
+    private String backgroundColor;
+    private String border;
+    
+    public ColorBean() {
+        
+    }
+
+    /**
+     * @return the foregroundColor
+     */
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    /**
+     * @param foregroundColor the foregroundColor to set
+     */
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    /**
+     * @return the backgroundColor
+     */
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * @param backgroundColor the backgroundColor to set
+     */
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    /**
+     * @return the border
+     */
+    public String getBorder() {
+        return border;
+    }
+
+    /**
+     * @param border the border to set
+     */
+    public void setBorder(String border) {
+        this.border = border;
+    }
+    
+    
+}
