bcf94702adc0650eae86482c906658706216d4bb
Kamil Osak <osak.k98@gmail.com>
2022-06-04 16:35:21 +0200

    lab8

---
+++ Web Services (REST) ASP.NET Web API/MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+        [Required(ErrorMessage = "Required"),
+         StringLength(140, MinimumLength = 5, ErrorMessage = "Minimum 5 characters, maximum 140 characters.")]
+        public string Text { get; set; } = string.Empty;
+
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
